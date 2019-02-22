687d7b3e72111e770c9a3302ef445d234c9950cd
btbell <btbell@gmail.com>
2019-02-22 16:42:04 -0500

    added state constructor example on App.js

---
+++ src/components/SearchAppointment.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchAppointment extends Component {
+	render() {
+		return <div>Search Appointments</div>
+	}
+}
+
+export default SearchAppointment;
