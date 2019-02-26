f3ba5692f60d95266a2a28641fed6ac56d3bb648
btbell <btbell@gmail.com>
2019-02-26 13:39:52 -0500

    pulled data via fetch, created an array to display this data

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,33 @@ import ListAppointment from './ListAppointment';
 class App extends Component {
 
   constructor() {
-  super();
-  this.state = {
-    myName: 'Brian'
+    super();
+    this.state = {
+      myName: 'Brian',
+      myAppointments: []
+    };
   }
-}
+
+  componentDidMount() {
+    fetch('./data.json') 
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+     
+  }
 
   render() {
+
+    const listItems = this.state.myAppointments.map(item => (
+      <div>{item.petName}</div>
+      ));
+
     return (
        <main className="page bg-white" id="petratings">
         <div className="container">
@@ -22,6 +42,7 @@ class App extends Component {
             <div className="col-md-12 bg-white">
               <div className="container">
                 { this.state.myName }
+                {listItems}
                 <AddAppointment />
                 <SearchAppointment />
                 <ListAppointment />
