7199dca516aef6ae560abf74c0dd2d9c4b831462
btbell <btbell@gmail.com>
2019-03-07 16:48:53 -0500

    added code to render into the template

--- src/components/ListAppointment.js
+++ src/components/ListAppointment.js
@@ -6,7 +6,30 @@ render() {
     <div>{item.petName}</div>
     ));
 
-	return <div>{listItems}</div>
+	return (
+		<div className="appointment-list item-list mb-3">
+		  {this.props.appointment.map(item => (
+			<div className="pet-item col media py-3">
+          	  <div className="mr-3">
+              	<button className="pet-delete btn btn-sm btn-danger">X</button>
+          	</div>
+
+          	<div className="pet-info media-body">
+            	<div className="pet-head d-flex">
+              	  <span className="pet-name">{item.petName}</span>
+                  <span className="apt-date ml-auto">{item.aptDate}</span>
+            	</div>
+
+            <div className="owner-name">
+              <span className="label-item">Owner: </span>
+              <span>ownerName</span>
+            </div>
+          	<div className="apt-notes">aptNotes</div>
+          </div>
+        </div>
+			))}
+      </div>
+	);
   }
 }
 
