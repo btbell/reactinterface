f88af6f36c3a32376b4b4a027a4f057a4cfc1ceb
btbell <btbell@gmail.com>
2019-04-19 15:36:31 -0400

    wrapped up sorting code

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,8 @@ class App extends Component {
     this.state = {
       myAppointment: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDirection: 'asc',
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
@@ -54,7 +56,7 @@ class App extends Component {
           item.aptId = this.state.lastIndex;
           this.setState({ lastIndex: this.state.lastIndex +1});
           return item;
-        })
+        });
         this.setState({
           myAppointment: apts
         });
@@ -62,6 +64,25 @@ class App extends Component {
   }
 
   render() {
+
+    let order;
+    let filteredApts = this.state.myAppointment;
+    if(this.state.orderDirection === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() <
+        b[this.state.orderBy].toLowerCase()
+      ) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    });
+
     return (
        <main className="page bg-white" id="petratings">
         <div className="container">
@@ -74,8 +95,9 @@ class App extends Component {
                   addAppointment = {this.addAppointment}
                 />
                 <SearchAppointment />
-                <ListAppointment appointment={this.state.myAppointment}
-                deleteAppointment={this.deleteAppointment}/>
+                <ListAppointment
+                  appointment={filteredApts}
+                  deleteAppointment={this.deleteAppointment}/>
               </div>
             </div>
           </div>
